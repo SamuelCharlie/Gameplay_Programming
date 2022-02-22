31a9c2614c8550b2b2db86e00d83c3f0037c698a
Samuel Tamkin <samuel2.tamkin@live.uwe.ac.uk>
2022-02-22 08:47:20 +0000

    Began Animations

--- Gameplay Programming Movement/Assets/Scripts/PlayerController.cs
+++ Gameplay Programming Movement/Assets/Scripts/PlayerController.cs
@@ -7,23 +7,41 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
 
-    float horizontal_move = Input.GetAxis("Horizontal");
-    float vertical_move = Input.GetAxis("Vertical");
-    public float player_speed = 1.0f;
+    private float x_move = 0.0f;
+    private float y_move = 0.0f;
+    private float x_max_velocity = 40.0f;
+    private float y_max_velocity = 40.0f;
 
-    private bool player_moving = false;
+    public float player_speed = 1.0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void OnMove(InputValue move_value)
+    {
+        Vector2 move_vector = move_value.Get<Vector2>();
+
+        x_move = move_vector.x;
+        y_move = move_vector.y;
+    }
+
     void FixedUpdate()
     {
-        if (player_moving)
+        Vector3 move = new Vector3(x_move, 0.0f, y_move);
+        rb.AddForce(move * player_speed);
+
+        if (rb.velocity.x >= x_max_velocity)
         {
-            Vector3 movement = new Vector3(horizontal_move, 0.0f, vertical_move);
-            rb.MovePosition(movement);
+            Debug.Log("x too fast");
+            rb.velocity = new Vector3(x_max_velocity, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (rb.velocity.y >= y_max_velocity)
+        {
+            Debug.Log("y too fast");
+            rb.velocity = new Vector3(rb.velocity.x, y_max_velocity, rb.velocity.z);
         }
     }
 }
